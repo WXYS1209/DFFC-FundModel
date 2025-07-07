9edf20bc1024ef67fb223bbce709edecf482f23b
Rick <xie_jinxiao@126.com>
2025-07-07 13:58:00 +0800

    更新了backtest的plot_result函数
    可以把策略用自己的名字做前缀扔进来，这样可以共享然后不覆盖

--- strategy_single.py
+++ strategy_single.py
@@ -36,16 +36,19 @@ class StrategyExample(BackTestFuncInfo):
     程序将自动按照交易信号执行买入、卖出等操作。
 
     1. 输入信息
-        a. 净值列表
-        净值数据列表可以通过self.strategy_list，第i个基金的的净值数据为self.strategy_list[i]: 
-        self.strategy_list[i][0]为日期列表，
-        self.strategy_list[i][1]为净值列表，
-        self.strategy_list[i][2]为holtwinters_delta_percentage数据
-        self.strategy_list[i][j][k]为k日前的数据，0表示日数据。
+        a. 基金数据列表
+        基金数据通过以下三个列表访问：
+        - self.strategy_unit_value_list[i] : 第i个基金的单位净值列表
+        - self.strategy_date_list[i] : 第i个基金的日期列表  
+        - self.strategy_factor_list[i] : 第i个基金的holtwinters_delta_percentage数据
+        
+        其中[i][j]表示第i个基金的第j天数据，j=0表示当天数据，j=1表示前1天数据，以此类推。
+        如果基金i在当前日期没有数据，则对应列表为None。
         也可以使用净值列表来每日都计算自定义的因子。
 
         b. 当前持仓信息
-            当前持仓信息可以通过self.current_asset获取，格式为[datetime, [cash, fund1shares, fund2shares, ...],[None(目前是一个空列表)] , [cash, fund1value, fund2value, ...]]
+            当前持仓信息可以通过self.current_asset获取，格式为：
+            [datetime, [cash, fund1shares, fund2shares, ...], [None(目前是一个空列表)], [cash, fund1value, fund2value, ...]]
             其中cash为当前现金，fund1shares为持有的基金1的份额，fund1value为基金1的当前价值。
 
             历史持仓信息self.asset_list是一个列表，包含了每一天的持仓信息，包含了每一日的current_asset信息。
@@ -55,7 +58,7 @@ class StrategyExample(BackTestFuncInfo):
             每日的交易由一个列表构成：
             [datetime, [sellfundNumber, buyfundNumber, sellshares, price], ...]
             datetime为交易日期，
-            sellfundNumber为卖出基金的编号(0表示现金, 1表示self.strategy_list[0]基金, 2表示self.strategy_list[1]基金, ...)，
+            sellfundNumber为卖出基金的编号(0表示现金, 1表示fund_list[0]基金, 2表示fund_list[1]基金, ...)，
             buyfundNumber为买入基金的编号，
             sellshares为卖出份额，
             price为交易价格。
