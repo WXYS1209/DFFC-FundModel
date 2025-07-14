1bfe920ca60d54fb7d0a7d8600c094505953dab8
Rick <xie_jinxiao@126.com>
2025-07-14 20:50:15 +0800

    上传了一个小方块调仓策略

--- rick_strategy_reallocation.py
+++ rick_strategy_reallocation.py
@@ -19,7 +19,7 @@ plt.rcParams['axes.unicode_minus'] = False
     - 周期平滑靠拢目标仓位：每次调仓时，按照当前持仓和目标仓位的差值进行调整，调整差值的0.5(可调参数)倍率
     - 事件驱动调仓：如果当前持仓和目标仓位的差值很大，则需要相应减小调仓量？（追涨杀跌？）
 
-磁滞回线？用短期动量+位置判断调仓量
+磁滞回线？用短期动量+位置判断调仓量，不加入动量判断就没用
 
 '''
 
@@ -32,9 +32,12 @@ class StrategyExample(BackTestFuncInfo):
         super().__init__(fund_list, start_date, end_date)
 
         # 目标仓位列表
-        self.target_position = [0.7, 0.3]
+        self.target_position = [0.5, 0.5]
         # 归一化目标仓位
         self.target_position = [x / sum(self.target_position) for x in self.target_position]
+        # 初始化目标仓位记忆开关
+        self.memory_switch = True
+        self.memory_target_position = deepcopy(self.target_position)
 
     # 重写策略函数
     def strategy_func(self):
@@ -51,10 +54,39 @@ class StrategyExample(BackTestFuncInfo):
             return operation_list
             
         # 如果不是回测开始日期，则需要进行调仓
-        # 首先基于当前HDP算目标仓位
-        target_position_hdp = [self.target_position[i] / (1 + self.strategy_factor_list[i][0]+0.00001) for i in range(len(self.target_position))]  # 计算目标仓位的HDP
-        # 归一化目标仓位
-        target_position_hdp = [x / sum(target_position_hdp) for x in target_position_hdp]
+        # 1. 首先基于当前HDP算目标仓位，并归一化
+        #target_position_hdp = [self.target_position[i] / (1 + self.strategy_factor_list[i][0]+0.00001) for i in range(len(self.target_position))]  # 计算目标仓位的HDP
+        #target_position_hdp = [x / sum(target_position_hdp) for x in target_position_hdp]
+
+        # 2. 基于HDP的差值计算目标位
+        #deltahdp = self.strategy_factor_list[0][0] - self.strategy_factor_list[1][0]  # 计算HDP差值
+        #memorythreshold = 0.6
+        #if not self.memory_switch and deltahdp > memorythreshold:
+        #    self.memory_switch = True  # 如果HDP差值大于阈值，则开启记忆开关
+        #elif self.memory_switch and deltahdp < -memorythreshold:
+        #    self.memory_switch = False
+        # 如果记忆开关开启，则使用HDP差值计算目标仓位
+        #if self.memory_switch:
+        #    if deltahdp > memorythreshold:
+        #        target_position_hdp = [0.5 - deltahdp/4, 0.5 + deltahdp/4]  # 如果HDP差值大于阈值，则调整目标仓位
+        #    elif deltahdp <= memorythreshold:
+        #        target_position_hdp = [0.5 - memorythreshold/4, 0.5 + memorythreshold/4]  # 如果HDP差值小于等于阈值，则调整目标仓位
+        #else:
+        #    if deltahdp < -memorythreshold:
+        #        target_position_hdp = [0.5 - deltahdp/4, 0.5 + deltahdp/4]  # 如果HDP差值小于阈值，则调整目标仓位
+        #    elif deltahdp >= -memorythreshold:
+        #        target_position_hdp = [0.5 + memorythreshold/4, 0.5 - memorythreshold/4]
+        
+        # 3. 极简版磁滞回线逻辑
+        deltahdp = self.strategy_factor_list[0][0] - self.strategy_factor_list[1][0]  # 计算HDP差值
+        if self.memory_switch and deltahdp > 0.7:
+            self.memory_target_position = [0.1, 0.9]  # 更新记忆目标仓位
+            self.memory_switch = False
+        elif not self.memory_switch and deltahdp < -0.7:
+            self.memory_target_position = [0.9, 0.1]  # 更新记忆目标仓位
+            self.memory_switch = True
+        target_position_hdp = deepcopy(self.memory_target_position)
+
 
         # 计算当前持仓价值
         currentprice = [self.current_asset[1][i+1]* self.strategy_unit_value_list[i][0] for i in range(len(self.target_position))]
@@ -64,7 +96,7 @@ class StrategyExample(BackTestFuncInfo):
         diffshare = [diffprice[i] / self.strategy_unit_value_list[i][0] for i in range(len(diffprice))]  # 转换为份额差值   
 
         # 计算调仓量，按照差值的0.5倍进行调整
-        adjust_factor = 0.9  # 可调参数，调整差值的倍率
+        adjust_factor = 0.1  # 可调参数，调整差值的倍率
         adjust_diffshare = [diffshare[i]*adjust_factor for i in range(len(diffshare))]  # 调整差值
 
         # 初始化买入和卖出数量
@@ -84,12 +116,13 @@ if __name__ == "__main__":
     etflist = ExtendedFuncInfo.create_fundlist_config("fund_config_dual.json")
     for fund in etflist:
         fund.load_data_csv(f"./csv_data/{fund.code}.csv")  # 从本地CSV文件加载数据
+        #fund.load_data_net()  # 从网络加载数据
         fund.factor_cal_holtwinters()
         fund.factor_cal_holtwinters_delta_percentage()
         fund.set_info_dict()
     print("==========================================================")
 
     # 运行策略回测
-    strategy = StrategyExample(etflist, start_date=datetime(2024, 7, 1), end_date=datetime(2025, 7, 1))
+    strategy = StrategyExample(etflist, start_date=datetime(2022, 7, 1), end_date=datetime(2025, 7, 1))
     strategy.run()
     strategy.plot_result()
